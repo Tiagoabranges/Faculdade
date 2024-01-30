bd54f6f9bf4631b6b9b23d5c0d7c0d7bca55979d
tiagoabranges <tiagoabranges@gmail.com>
2024-01-30 16:26:55 -0300

    feat: button php

--- Php/moto.php
+++ Php/moto.php
@@ -1,13 +1,3 @@
-<?php
 
-$motos = Array("yamaha", "fan", "titan");
-
-foreach($motos as $moto){
-   echo $moto;
-   echo "\n";
-}
-
-for ($i = 0; $i < count($motos); $i++) {
-   echo $motos[$i];
-   echo "\n";
-}
+Hi <?php echo htmlspecialchars($_POST['name']); ?>.
+You are <?php echo (int)$_POST['age']; ?> years old.
