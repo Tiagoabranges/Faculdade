4826fea0433bab5300930a5f0a0d9d824df0359d
tiagoabranges <tiagoabranges@gmail.com>
2024-01-30 16:27:59 -0300

    feat: implement file car php

--- Php/carro.php
+++ Php/carro.php
@@ -1,13 +1,22 @@
 <?php
+echo 'isto é uma string comum';
 
-$carros = Array("Fusca", "gol", "Passat");
+echo 'Você pode incluir novas linhas em strings,
+dessa maneira que estará
+tudo bem';
 
-foreach($carros as $carro){
-   echo $carro;
-   echo "\n";
-}
+// Imprime: Arnold disse uma vez: "I'll be back"
+echo 'Arnold disse uma vez: "I\'ll be back"';
 
-for ($i = 0; $i < count($carros); $i++) {
-   echo $carros[$i];
-   echo "\n";
-}
+// Imprime: Você tem certeza em apagar C:\*.*?
+echo 'Você tem certeza em apagar C:\\*.*?';
+
+// Imprime: Você tem certeza em apagar C:\*.*?
+echo 'Você tem certeza em apagar C:\*.*?';
+
+// Imprime: Isto não será substituído: \n uma nova linha
+echo 'Isto não será substituído: \n uma nova linha';
+
+// Imprime: Variáveis $também não $expandem
+echo 'Variáveis $também não $expandem';
+?>
