e4ed3fa93c86f82f8da26b7c2ce8a2218de5a0c6
tiagoabranges <tiagoabranges@gmail.com>
2024-01-30 16:22:04 -0300

    feat: criado arquivo de motos php

---
+++ Php/moto.php
@@ -0,0 +1,13 @@
+<?php
+
+$motos = Array("yamaha", "fan", "titan");
+
+foreach($motos as $moto){
+   echo $moto;
+   echo "\n";
+}
+
+for ($i = 0; $i < count($motos); $i++) {
+   echo $motos[$i];
+   echo "\n";
+}
